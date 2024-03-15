a71587a05263561fa70b66f563b6a36aee78370d
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-15 23:03:11 +0530

    Update Youtube.py

--- Wifemusic/platforms/Youtube.py
+++ Wifemusic/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from AnonXMusic.utils.database import is_on_off
-from AnonXMusic.utils.formatters import time_to_seconds
+from Wifemusic.utils.database import is_on_off
+from Wifemusic.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
