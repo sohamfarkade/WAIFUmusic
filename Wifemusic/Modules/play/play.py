713e7b28016979a983497f2a91d0a28cc4161620
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-15 21:23:53 +0530

    Update play.py

--- Wifemusic/Modules/play/play.py
+++ Wifemusic/Modules/play/play.py
@@ -6,22 +6,22 @@ from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from AnonXMusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils import seconds_to_min, time_to_seconds
-from AnonXMusic.utils.channelplay import get_channeplayCB
-from AnonXMusic.utils.decorators.language import languageCB
-from AnonXMusic.utils.decorators.play import PlayWrapper
-from AnonXMusic.utils.formatters import formats
-from AnonXMusic.utils.inline import (
+from Wifemusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
+from Wifemusic.core.call import Wife
+from Wifemusic.utils import seconds_to_min, time_to_seconds
+from Wifemusic.utils.channelplay import get_channeplayCB
+from Wifemusic.utils.decorators.language import languageCB
+from Wifemusic.utils.decorators.play import PlayWrapper
+from Wifemusic.utils.formatters import formats
+from Wifemusic.utils.inline import (
     botplaylist_markup,
     livestream_markup,
     playlist_markup,
     slider_markup,
     track_markup,
 )
-from AnonXMusic.utils.logger import play_logs
-from AnonXMusic.utils.stream.stream import stream
+from Wifemusic.utils.logger import play_logs
+from Wifemusic.utils.stream.stream import stream
 from config import BANNED_USERS, lyrical
 
 
