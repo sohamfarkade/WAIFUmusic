6116344bd3ca233322a966e30b91778eef41c354
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-15 21:38:49 +0530

    Update maintenance.py

--- Wifemusic/Modules/sudo/maintenance.py
+++ Wifemusic/Modules/sudo/maintenance.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import (
+from Wifemusic import app
+from Wifemusic.misc import SUDOERS
+from Wifemusic.utils.database import (
     get_lang,
     is_maintenance,
     maintenance_off,
