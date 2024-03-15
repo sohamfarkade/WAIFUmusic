dc61ac8b38f0f63a5310036596f67e5824da5c7a
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-15 21:29:42 +0530

    Update autoend.py

--- Wifemusic/Modules/sudo/autoend.py
+++ Wifemusic/Modules/sudo/autoend.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import autoend_off, autoend_on
+from Wifemusic import app
+from Wifemusic.misc import SUDOERS
+from Wifemusic.utils.database import autoend_off, autoend_on
 
 
 @app.on_message(filters.command("autoend") & SUDOERS)
