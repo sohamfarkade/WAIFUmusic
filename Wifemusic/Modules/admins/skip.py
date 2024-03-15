98e90a41ada78c57db258c6ec65b233ba1291a7b
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-15 20:51:18 +0530

    Update skip.py

--- Wifemusic/Modules/admins/skip.py
+++ Wifemusic/Modules/admins/skip.py
@@ -86,7 +86,7 @@ async def skip(cli, message: Message, _, chat_id):
                     ),
                     reply_markup=close_markup(_),
                 )
-                return await Anony.stop_stream(chat_id)
+                return await Wife.stop_stream(chat_id)
             except:
                 return
     queued = check[0]["file"]
@@ -144,7 +144,7 @@ async def skip(cli, message: Message, _, chat_id):
         except:
             image = None
         try:
-            await Anony.skip_stream(chat_id, file_path, video=status, image=image)
+            await Wife.skip_stream(chat_id, file_path, video=status, image=image)
         except:
             return await mystic.edit_text(_["call_6"])
         button = stream_markup(_, chat_id)
