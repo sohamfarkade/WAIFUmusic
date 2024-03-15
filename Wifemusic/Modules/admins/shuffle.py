ad077b2bad57aac706523d2bbc04a3f2f3d8426b
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-15 20:46:07 +0530

    Update shuffle.py

--- Wifemusic/Modules/admins/shuffle.py
+++ Wifemusic/Modules/admins/shuffle.py
@@ -3,10 +3,10 @@ import random
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import db
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from Wifemusic import app
+from Wifemusic.misc import db
+from Wifemusic.utils.decorators import AdminRightsCheck
+from Wifemusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
