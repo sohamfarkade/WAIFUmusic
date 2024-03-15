ce7c6d5b1d98d71da4f9ae9767076abeb364e7e5
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-15 23:41:26 +0530

    Update logger.py

--- Wifemusic/utils/logger.py
+++ Wifemusic/utils/logger.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import ParseMode
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import is_on_off
+from Wifemusic import app
+from Wifemusic.utils.database import is_on_off
 from config import LOGGER_ID
 
 
