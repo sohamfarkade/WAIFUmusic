1fee809948d52967411b081f0d40bdf9618a5a26
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-15 23:31:53 +0530

    Update channelplay.py

--- Wifemusic/utils/channelplay.py
+++ Wifemusic/utils/channelplay.py
@@ -1,5 +1,5 @@
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_cmode
+from Wifemusic import app
+from Wifemusic.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
