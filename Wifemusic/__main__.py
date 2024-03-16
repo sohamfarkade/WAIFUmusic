b880f12121df68702ae4609fc1fe2b519e88f4ec
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-16 14:56:18 +0530

    Update __main__.py

--- Wifemusic/__main__.py
+++ Wifemusic/__main__.py
@@ -1,19 +1,33 @@
 import asyncio
 import importlib
+import random
+import time
 
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CommandHandler, CallbackContext, MessageHandler, filters
 
 import config
-from Wifemusic import LOGGER, app, userbot
+from wifemusic import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection, Wifemusic
+from Wifemusic import application, SUPPORT_CHAT, UPDATE_CHAT, db, LOGGER
+
+from Wifemusic import LOGGER, app as wife_app, userbot
 from Wifemusic.core.call import Anony
 from Wifemusic.misc import sudo
-from Wifemusic.Modules import ALL_MODULES
+from Wifemusic.Modules import ALL_MODULES as WifeModules
 from Wifemusic.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
+locks = {}
+message_counters = {}
+spam_counters = {}
+last_characters = {}
+sent_characters = {}
+first_correct_guesses = {}
+message_counts = {}
 
-async def init():
+async def init_wifemusic():
     if (
         not config.STRING1
         and not config.STRING2
@@ -33,8 +47,8 @@ async def init():
             BANNED_USERS.add(user_id)
     except:
         pass
-    await app.start()
-    for all_module in ALL_MODULES:
+    await wife_app.start()
+    for all_module in WifeModules:
         importlib.import_module("Wifemusic.Modules" + all_module)
     LOGGER("Wifemusic.Modules").info("Successfully Imported Modules...")
     await userbot.start()
@@ -53,10 +67,98 @@ async def init():
         "\x41\x6e\x6f\x6e\x58\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\n\n\x44\x6f\x6e'\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x46\x61\x6c\x6c\x65\x6e\x41\x73\x73\x6f\x63\x69\x61\x74\x69\x6f\x6e"
     )
     await idle()
-    await app.stop()
+    await wife_app.stop()
     await userbot.stop()
     LOGGER("Wifemusic").info("Stopping Wife Music Bot...")
 
+async def init_sanatan():
+    Sanatan.start()
+    LOGGER.info("Sanatan Bot started")
+
+async def message_counter(update: Update, context: CallbackContext) -> None:
+    chat_id = str(update.effective_chat.id)
+    user_id = update.effective_user.id
+
+    if chat_id not in locks:
+        locks[chat_id] = asyncio.Lock()
+    lock = locks[chat_id]
+
+    async with lock:
+        
+        chat_frequency = await user_totals_collection.find_one({'chat_id': chat_id})
+        if chat_frequency:
+            message_frequency = chat_frequency.get('message_frequency', 100)
+        else:
+            message_frequency = 100
+
+        
+        if chat_id in last_user and last_user[chat_id]['user_id'] == user_id:
+            last_user[chat_id]['count'] += 1
+            if last_user[chat_id]['count'] >= 10:
+            
+                if user_id in warned_users and time.time() - warned_users[user_id] < 600:
+                    return
+                else:
+                    
+                    await update.message.reply_text(f"⚠️ Don't Spam {update.effective_user.first_name}...\nYour Messages Will be ignored for 10 Minutes...")
+                    warned_users[user_id] = time.time()
+                    return
+        else:
+            last_user[chat_id] = {'user_id': user_id, 'count': 1}
+
+    
+        if chat_id in message_counts:
+            message_counts[chat_id] += 1
+        else:
+            message_counts[chat_id] = 1
+
+    
+        if message_counts[chat_id] % message_frequency == 0:
+            await send_image(update, context)
+            
+            message_counts[chat_id] = 0
+
+async def send_image(update: Update, context: CallbackContext) -> None:
+    chat_id = update.effective_chat.id
+
+    all_characters = list(await collection.find({}).to_list(length=None))
+    
+    if chat_id not in sent_characters:
+        sent_characters[chat_id] = []
+
+    if len(sent_characters[chat_id]) == len(all_characters):
+        sent_characters[chat_id] = []
+
+    character = random.choice([c for c in all_characters if c['id'] not in sent_characters[chat_id]])
+
+    sent_characters[chat_id].append(character['id'])
+    last_characters[chat_id] = character
+
+    if chat_id in first_correct_guesses:
+        del first_correct_guesses[chat_id]
+
+    await context.bot.send_photo(
+        chat_id=chat_id,
+        photo=character['img_url'],
+        caption=f"""A New {character['rarity']} Character Appeared...\n/guess Character Name and add in Your Harem""",
+        parse_mode='Markdown')
+
+async def guess(update: Update, context: CallbackContext) -> None:
+    # Guess function from Sanatan bot
+    pass
+
+async def fav(update: Update, context: CallbackContext) -> None:
+    # Fav function from Sanatan bot
+    pass
+
+def main() -> None:
+    """Run bot."""
+    application.add_handler(CommandHandler(["guess", "protecc", "collect", "grab", "hunt"], guess, block=False))
+    application.add_handler(CommandHandler("fav", fav, block=False))
+    application.add_handler(MessageHandler(filters.ALL, message_counter, block=False))
+    application.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(init())
+    asyncio.get_event_loop().run_until_complete(init_sanatan())
+    asyncio.get_event_loop().run_until_complete(init_wifemusic())
+    main()
