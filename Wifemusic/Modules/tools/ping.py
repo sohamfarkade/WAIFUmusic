a364019270c5e4b70d7935232688206d67c1f010
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-16 12:46:08 +0530

    Update ping.py

--- Wifemusic/Modules/tools/ping.py
+++ Wifemusic/Modules/tools/ping.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from Wifemusic import app
-from Wifemusic.core.call import Wife
+from Wifemusic.core.call import Anony
 from Wifemusic.utils import bot_sys_stats
 from Wifemusic.utils.decorators.language import language
 from Wifemusic.utils.inline import supp_markup
@@ -19,7 +19,7 @@ async def ping_com(client, message: Message, _):
         photo=PING_IMG_URL,
         caption=_["ping_1"].format(app.mention),
     )
-    pytgping = await Wife.ping()
+    pytgping = await Anony.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
