52735f44fdfd8a320b3b730acc25996c6d6e536f
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-16 09:09:47 +0530

    Update call.py

--- Wifemusic/core/call.py
+++ Wifemusic/core/call.py
@@ -33,7 +33,7 @@ from Wifemusic.utils.database import (
 )
 from Wifemusic.utils.exceptions import AssistantErr
 from Wifemusic.utils.formatters import check_duration, seconds_to_min, speed_converter
-from WifemMusic.utils.inline.play import stream_markup
+from Wifemusic.utils.inline.play import stream_markup
 from Wifemusic.utils.stream.autoclear import auto_clean
 from Wifemusic.utils.thumbnails import get_thumb
 from strings import get_string
