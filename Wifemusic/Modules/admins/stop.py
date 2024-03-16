4cd9ad1e90716e4b720744c5a262dc5e923c9db8
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-16 11:04:46 +0530

    Update stop.py

--- Wifemusic/Modules/admins/stop.py
+++ Wifemusic/Modules/admins/stop.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from Wifemusic import app
-from Wifemusic.core.call import Wife
+from Wifemusic.core.call import Anony
 from Wifemusic.utils.database import set_loop
 from Wifemusic.utils.decorators import AdminRightsCheck
 from Wifemusic.utils.inline import close_markup
@@ -16,7 +16,7 @@ from config import BANNED_USERS
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return
-    await Wife.stop_stream(chat_id)
+    await Anony.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
