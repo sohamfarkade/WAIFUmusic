71523a341f5794559f51c5a9b4c0fe92aa64fb85
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-17 08:52:32 +0530

    Update image.py

--- Wifemusic/Module/image.py
+++ Wifemusic/Module/image.py
@@ -12,7 +12,7 @@ from pyrogram.types import (
 from pyrogram import Client, filters
 
 # Replace "Grabber" with your actual module name.
-from Sanatan import Sanatan
+from Wifemusic import Wifemusic
 
 ENDPOINT = "https://sasta-api.vercel.app/googleImageSearch"
 httpx_client = httpx.AsyncClient(timeout=60)
