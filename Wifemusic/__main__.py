7ad682e257ef2a5234491a287a6401efe81bcf73
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-22 18:50:18 +0530

    Update __main__.py

--- Wifemusic/__main__.py
+++ Wifemusic/__main__.py
@@ -35,8 +35,8 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("Wifemusicusic.modules" + all_module)
-    LOGGER("Wifemusic.modules").info("Successfully Imported Modules...")
+        importlib.import_module("Wifemusicusic.Modules" + all_module)
+    LOGGER("Wifemusic.Modules").info("Successfully Imported Modules...")
     await userbot.start()
     await Anony.start()
     try:
