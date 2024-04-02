a6339ce7e661e102e53b0e8555c70bdd4d72679c
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-02 14:02:23 +0530

    Update __init__.py

--- Sanatan/__init__.py
+++ Sanatan/__init__.py
@@ -15,7 +15,7 @@ logging.getLogger('httpx').setLevel(logging.WARNING)
 logging.getLogger("pyrate_limiter").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
-from @WAIFUmusic.Config import Development as Config
+from Sanatan.Config import Development as Config
 
 
 API_ID= Config.API_ID
