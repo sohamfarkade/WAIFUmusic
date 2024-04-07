ad2c1101366c3f41a436e9e8721d72418e15105c
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-07 21:19:09 +0530

    Update leaderboard.py

--- Wifemusic/Modules/Waifu/leaderboard.py
+++ Wifemusic/Modules/Waifu/leaderboard.py
@@ -5,11 +5,12 @@ import html
 from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext
 
-from Sanatan import (application, VIDEO_URL, OWNER_ID,
+from . import OWNER_ID
+from Wifemusic import (application, VIDEO_URL, OWNER_ID,
                     user_collection, top_global_groups_collection, top_global_groups_collection, 
                     group_user_totals_collection)
 
-from Sanatan import sudo_users as SUDO_USERS 
+from Wifemusic.misc import SUDOERS
 
     
 async def global_leaderboard(update: Update, context: CallbackContext) -> None:
