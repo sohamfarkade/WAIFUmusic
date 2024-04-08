14a8269f7ab56744d1d2134ce5b45d004754072d
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-08 14:42:37 +0530

    Update __init__.py

--- Wifemusic/__init__.py
+++ Wifemusic/__init__.py
@@ -24,31 +24,3 @@ Spotify = SpotifyAPI()
 Resso = RessoAPI()
 Telegram = TeleAPI()
 YouTube = YouTubeAPI()
-import logging  
-import os
-from pyrogram import Client 
-from telegram.ext import Application
-from motor.motor_asyncio import AsyncIOMotorClient
-
-logging.basicConfig(
-    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
-    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
-    level=logging.INFO,
-)
-
-logging.getLogger("apscheduler").setLevel(logging.ERROR)
-logging.getLogger('httpx').setLevel(logging.WARNING)
-logging.getLogger("pyrate_limiter").setLevel(logging.ERROR)
-LOGGER = logging.getLogger(__name__) 
-
-
-application = Application.builder().token(BOT_TOKEN).build()
-Wifemusic = Client("wifemusic", API_ID, API_HASH, bot_token=BOT_TOKEN)
-lol = AsyncIOMotorClient(MONGO_URL)
-db = lol['Character_catcher']
-collection = db['anime_characters_lol']
-user_totals_collection = db['user_totals_lmaoooo']
-user_collection = db["user_collection_lmaoooo"]
-group_user_totals_collection = db['group_user_totalsssssss']
-top_global_groups_collection = db['top_global_groups']
-pm_users = db['total_pm_users']
