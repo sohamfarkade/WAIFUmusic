01e35c861121b51874075bdd9cb291683f6fc00b
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-09 15:01:42 +0530

    Update __init__.py

--- Wifemusic/Modules/Waifu/__init__.py
+++ Wifemusic/Modules/Waifu/__init__.py
@@ -55,7 +55,7 @@ def __list_all_modules():
             to_load = all_modules
 
         if NO_LOAD:
-            LOGGER.info("Not loading: {}".format(NO_LOAD))
+            LOGGER(__name__).info("Not loading: {}".format(NO_LOAD))
             return [item for item in to_load if item not in NO_LOAD]
 
         return to_load
@@ -64,5 +64,5 @@ def __list_all_modules():
 
 
 ALL_MODULES = __list_all_modules()
-LOGGER.info("Modules to load: %s", str(ALL_MODULES))
+LOGGER(__name__).info("Modules to load: %s", str(ALL_MODULES))
 __all__ = ALL_MODULES + ["ALL_MODULES"]
