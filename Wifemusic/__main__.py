d9278a3cfef5c05cc6123f8f5fd42c0f699a0c35
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-10 20:12:56 +0530

    Update __main__.py

--- Wifemusic/__main__.py
+++ Wifemusic/__main__.py
@@ -1,98 +1,62 @@
 import asyncio
 import importlib
-import logging
 
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from Wifemusic import app, userbot
+from Wifemusic import LOGGER, app, userbot
 from Wifemusic.core.call import Anony
 from Wifemusic.misc import sudo
-from Wifemusic.Modules import *
+from Wifemusic.Modules import ALL_MODULES
 from Wifemusic.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
 
-locks = {}
-last_user = {}
-warned_users = {}
-message_counts = {}
-sent_characters = {}
-last_characters = {}
-first_correct_guesses = {}
-
-BANNED_USERS = config.BANNED_USERS  # Assuming this is defined in config.py
-
-LOGGER = logging.getLogger(__name__)
 
 async def init():
-    if not all(getattr(config, f"STRING{i+1}", None) for i in range(5)):
-        LOGGER.error("Assistant client variables not defined, exiting...")
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
         exit()
-
     await sudo()
-
     try:
         users = await get_gbanned()
-        BANNED_USERS.update(users)
-
+        for user_id in users:
+            BANNED_USERS.add(user_id)
         users = await get_banned_users()
-        BANNED_USERS.update(users)
-    except Exception as e:
-        LOGGER.error(f"Error while retrieving banned users: {e}")
-
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
     await app.start()
-
     for all_module in ALL_MODULES:
-        importlib.import_module("Wifemusic.Modules." + all_module)
-
-    LOGGER.info("Successfully Imported Modules...")
-
+        importlib.import_module("Wifemusic.Modules" + all_module)
+    LOGGER("Wifemusic.Modules").info("Successfully Imported Modules...")
     await userbot.start()
     await Anony.start()
-
     try:
         await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
-        LOGGER.error("Please turn on the video chat of your log group/channel.\n\nStopping Bot...")
+        LOGGER("Wifemusic").error(
+            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+        )
         exit()
-    except Exception as e:
-        LOGGER.error(f"Error while starting stream call: {e}")
-
+    except:
+        pass
     await Anony.decorators()
-
-    LOGGER.info("Bot started successfully.")
-
+    LOGGER("Wifemusic").info(
+        "\x41\x6e\x6f\x6e\x58\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\n\n\x44\x6f\x6e'\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x46\x61\x6c\x6c\x65\x6e\x41\x73\x73\x6f\x63\x69\x61\x74\x69\x6f\x6e"
+    )
     await idle()
-
     await app.stop()
     await userbot.stop()
+    LOGGER("Wifemusic").info("Stopping AnonX Music Bot...")
 
-    LOGGER.info("Stopping WAIFU Music Bot...")
-
-async def message_counter(update, context):
-    # Your message counter functionality here
-    pass
-
-async def send_image(update, context):
-    # Your send image functionality here
-    pass
-
-async def guess(update, context):
-    # Your guess functionality here
-    pass
-
-async def fav(update, context):
-    # Your favorite functionality here
-    pass
-
-def main():
-    """Run bot."""
-    application.add_handler(CommandHandler(["guess", "protecc", "collect", "grab", "hunt"], guess, run_async=True))
-    application.add_handler(CommandHandler("fav", fav, run_async=True))
-    application.add_handler(MessageHandler(Filters.all, message_counter, run_async=True))
-
-    application.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(init())
-    main()
