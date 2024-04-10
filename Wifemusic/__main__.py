547a4336ad2efe3a6ecbd4526c6daf21552ec1c0
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-10 20:50:01 +0530

    Update __main__.py

--- Wifemusic/__main__.py
+++ Wifemusic/__main__.py
@@ -34,7 +34,7 @@ async def init():
     except:
         pass
     await app.start()
-    for all_module in *:
+    for all_module in module_name:
         importlib.import_module("Wifemusic.Modules" + all_module)
     LOGGER("Wifemusic.Modules").info("Successfully Imported Modules...")
     await userbot.start()
