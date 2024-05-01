a98fbfa52229105b37b7e4c112005a899c02bdbd
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-05-01 12:02:14 +0530

    Update extraction.py

--- waifumusic/utils/extraction.py
+++ waifumusic/utils/extraction.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from AnonXMusic import app
+from waifumusic import app
 
 
 async def extract_user(m: Message) -> User:
