c0d0ea4db425c4e3dd060aa4c348b1c7eba7212e
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-05-01 14:09:59 +0530

    Update playmode.py

--- waifumusic/plugins/play/playmode.py
+++ waifumusic/plugins/play/playmode.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, Message
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_playmode, get_playtype, is_nonadmin_chat
-from AnonXMusic.utils.decorators import language
-from AnonXMusic.utils.inline.settings import playmode_users_markup
+from waifumusic import app
+from waifumusic.utils.database import get_playmode, get_playtype, is_nonadmin_chat
+from waifumusic.utils.decorators import language
+from waifumusic.utils.inline.settings import playmode_users_markup
 from config import BANNED_USERS
 
 
