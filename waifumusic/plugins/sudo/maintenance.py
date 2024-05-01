51a0036fd5954fd440a4b1665fb15224f31e9499
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-05-01 13:34:06 +0530

    Update maintenance.py

--- waifumusic/plugins/sudo/maintenance.py
+++ waifumusic/plugins/sudo/maintenance.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import (
+from waifumusic import app
+from waifumusic.misc import SUDOERS
+from waifumusic.utils.database import (
     get_lang,
     is_maintenance,
     maintenance_off,
