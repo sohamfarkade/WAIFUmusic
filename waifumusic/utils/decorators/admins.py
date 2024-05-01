195d75042f657c2f82742117a1f685c50602d0af
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-05-01 12:23:30 +0530

    Update admins.py

--- waifumusic/utils/decorators/admins.py
+++ waifumusic/utils/decorators/admins.py
@@ -1,9 +1,9 @@
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS, db
-from AnonXMusic.utils.database import (
+from waifumusic import app
+from waifumusic.misc import SUDOERS, db
+from waifumusic.utils.database import (
     get_authuser_names,
     get_cmode,
     get_lang,
