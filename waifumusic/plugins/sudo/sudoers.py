1b931fc5914bf5b1b8362f717eabcf3ac2fe571d
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-05-01 13:30:24 +0530

    Update sudoers.py

--- waifumusic/plugins/sudo/sudoers.py
+++ waifumusic/plugins/sudo/sudoers.py
@@ -1,12 +1,12 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import add_sudo, remove_sudo
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.extraction import extract_user
-from AnonXMusic.utils.inline import close_markup
+from waifumusic import app
+from waifumusic.misc import SUDOERS
+from waifumusic.utils.database import add_sudo, remove_sudo
+from waifumusic.utils.decorators.language import language
+from waifumusic.utils.extraction import extract_user
+from waifumusic.utils.inline import close_markup
 from config import BANNED_USERS, OWNER_ID
 
 
