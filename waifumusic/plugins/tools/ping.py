29e4755d3d94cac3b7401882ea3480e03e5ae05d
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-05-01 13:25:20 +0530

    Update ping.py

--- waifumusic/plugins/tools/ping.py
+++ waifumusic/plugins/tools/ping.py
@@ -3,11 +3,11 @@ from datetime import datetime
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils import bot_sys_stats
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.inline import supp_markup
+from waifumusic import app
+from waifumusic.core.call import Anony
+from waifumusic.utils import bot_sys_stats
+from waifumusic.utils.decorators.language import language
+from waifumusic.utils.inline import supp_markup
 from config import BANNED_USERS, PING_IMG_URL
 
 
@@ -19,7 +19,7 @@ async def ping_com(client, message: Message, _):
         photo=PING_IMG_URL,
         caption=_["ping_1"].format(app.mention),
     )
-    pytgping = await Anony.ping()
+    pytgping = await waifu.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
