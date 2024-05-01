aaf39afd43cd08512f415114c37dab0f59b7d59d
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-05-01 15:18:05 +0530

    Update resume.py

--- waifumusic/plugins/admins/resume.py
+++ waifumusic/plugins/admins/resume.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils.database import is_music_playing, music_on
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from waifumusic import app
+from waifumusic.core.call import Anony
+from waifumusic.utils.database import is_music_playing, music_on
+from waifumusic.utils.decorators import AdminRightsCheck
+from waifumusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -15,7 +15,7 @@ async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
-    await Anony.resume_stream(chat_id)
+    await waifu.resume_stream(chat_id)
     await message.reply_text(
         _["admin_4"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
