36ed577d1bf90dad88b9d87c75349c3b9c1e64a1
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-05-01 15:20:41 +0530

    Update loop.py

--- waifumusic/plugins/admins/loop.py
+++ waifumusic/plugins/admins/loop.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_loop, set_loop
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from waifumusic import app
+from waifumusic.utils.database import get_loop, set_loop
+from waifumusic.utils.decorators import AdminRightsCheck
+from waifumusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
