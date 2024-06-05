df699ca1ec595e63522f6d3a820d6fd31bfa79fb
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-06-05 15:17:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -68,7 +68,7 @@ GIT_TOKEN = getenv(
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/channelz_k")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/goku_groupz")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ayush_musix_support")
 # groupz-------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
